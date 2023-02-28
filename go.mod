91f3a79b662f275ddb30cefd4ba5e0d7a4943e53
Mr.X <mr.x@MrXs-MacBook-Pro.local>
2023-03-01 00:40:13 +0700

    release v0.0.1

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vnhacker1337/goutils
 
 go 1.19
+
+require github.com/Jeffail/gabs v1.4.0
---
+++ helpers/cmd.go
@@ -0,0 +1,68 @@
+package cmdutils
+
+import (
+	"bufio"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func ExecutionWithStd(cmd string) (string, error) {
+	command := []string{
+		"bash",
+		"-c",
+		cmd,
+	}
+	var output string
+	realCmd := exec.Command(command[0], command[1:]...)
+	// output command output to std too
+	cmdReader, _ := realCmd.StdoutPipe()
+	scanner := bufio.NewScanner(cmdReader)
+	var out string
+	go func() {
+		for scanner.Scan() {
+			out += scanner.Text()
+			//fmt.Fprintf(os.Stderr, scanner.Text()+"\n")
+			fmt.Println(scanner.Text())
+		}
+	}()
+	if err := realCmd.Start(); err != nil {
+		return "", err
+	}
+	if err := realCmd.Wait(); err != nil {
+		return "", err
+	}
+	return output, nil
+}
+
+func RuncmdWithLabel(cmd string, label string) (string, error) {
+	command := []string{
+		"bash",
+		"-c",
+		cmd,
+	}
+	fmt.Println("Start run Command: " + label)
+	var output string
+	realCmd := exec.Command(command[0], command[1:]...)
+	// output command output to std too
+
+	output = label
+	fmt.Println("Completed Run Command " + label)
+	if err := realCmd.Start(); err != nil {
+		return "", err
+	}
+	if err := realCmd.Wait(); err != nil {
+		return "", err
+	}
+	return output, nil
+}
+
+func InputCmd(Cmd string) string {
+	command := []string{
+		"bash",
+		"-c",
+		Cmd,
+	}
+	out, _ := exec.Command(command[0], command[1:]...).CombinedOutput()
+	return strings.TrimSpace(string(out))
+}
